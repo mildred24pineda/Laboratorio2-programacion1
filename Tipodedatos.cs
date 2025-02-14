548a1d0f9da93202fbfc873f4f4a143abba9bbc1
mildred24pineda <mpinedap3@miumg.edu.gt>
2025-02-13 23:46:07 -0600

    Add files via upload

---
+++ Tipodedatos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TiposdeDatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Número Entero (int)
+            int numentero = 12;
+            Console.WriteLine($"El Número Entero en (int) es: {numentero}");
+
+            // Número Decimal (float)
+            float decimalfloat = 3.1415927f;
+            Console.WriteLine($"El Número Decimal en (float) es: {decimalfloat}");
+
+            // Número Decimal (double)
+            double decimaldouble = 3.141592653589793;
+            Console.WriteLine($"El Número Decimal en (double) es: {decimaldouble}");
+
+            // Valor Máximo para byte
+            byte bytemax = 255;
+            Console.WriteLine($"El Valor Máximo en Byte es: {bytemax}");
+
+            // Texto
+            string texto = "Hola, Dios te Bendiga";
+            Console.WriteLine($"El Texto dice: {texto}");
+        }
+    }
+}
