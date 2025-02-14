bd15f3894d4ce9d843bd42ace9c617b5427bb670
mildred24pineda <mpinedap3@miumg.edu.gt>
2025-02-13 23:45:08 -0600

    Add files via upload

---
+++ Parte2 Operaciones Matematicas.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+
+namespace Parte2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operaciones con Enteros (int)
+            //Suma 
+            int sumaint = 6 + 8;
+            Console.WriteLine("El Resultado de la Suma (int) es:  " + sumaint);
+
+            //Resta 
+            int restaint = 3 + 7;
+            Console.WriteLine("El Resultado de la Resta (int) es:  " + restaint);
+
+            //Multiplicacion 
+            int multiplicacionint = 4 * 8;
+            Console.WriteLine("El Resultado de la Multiplicacion (int) es:  " + multiplicacionint);
+
+            //Division 
+            int divisionint = 16 / 4;
+            Console.WriteLine("El Resultado de la Division (int) es:  " + divisionint);
+
+
+
+            // Separador de operaciones
+            Console.WriteLine("====================================");
+
+
+
+            //Operaciones con Decimales (float)
+
+            //Suma
+            float sumafloat = 3.5f + 7.5f;
+            Console.WriteLine("El Resultado de la Suma (float) es:  " + sumafloat);
+
+            //Resta
+            float restafloat = 8.5f - 4.5f;
+            Console.WriteLine("El Resultado de la Resta (float) es:  " + restafloat);
+
+            //Multiplicacion
+            float multiplicacionfloat = 8.5f * 4.5f;
+            Console.WriteLine("El Resultado de la Multiplicacion (float) es:  " + multiplicacionfloat);
+
+            //Division
+            float divisionfloat = 10.5f / 2.5f;
+            Console.WriteLine("El Resultado de la Division (float) es:  " + divisionfloat);
+
+
+
+            // Separador de operaciones
+            Console.WriteLine("====================================");
+
+
+
+            //Operaciones con Decimales (double)
+            //Suma
+            double sumadouble = 8.5 + 3.5;
+            Console.WriteLine("El Resultado de la Suma (double) es: " + sumadouble);
+
+            //Resta
+            double restadouble = 10.5 - 5.5;
+            Console.WriteLine("El Resultado de la Resta (double) es: " + restadouble);
+
+            //Multiplicacion
+            double multiplicaciondouble = 2.5 * 6.5;
+            Console.WriteLine("El Resultado de la Multiplicaicon (double) es: " + multiplicaciondouble);
+
+            //Division
+            double divisiondouble = 20.5 * 4.5;
+            Console.WriteLine("El Resultado de la Division (double) es: " + divisiondouble);
+
+
+
+            // Separador de operaciones
+            Console.WriteLine("====================================");
+
+
+
+            //Operaciones con Enteros (int16)
+            //Suma
+            double sumaint16 = (Int16)(12 + 3);
+            Console.WriteLine("El Resultado de la Suma (in16) es: " + sumaint16);
+
+            //Resta
+            double restaint16 = (Int16)(25 - 8);
+            Console.WriteLine("El Resultado de la Resta (in16) es: " + restaint16);
+
+            //Multiplicacion
+            double multiplicacionint16 = (Int16)(38 * 4);
+            Console.WriteLine("El Resultado de la Multiplicacion (in16) es: " + multiplicacionint16);
+
+            //Division
+            double divisionint16 = (Int16)(38 / 7);
+            Console.WriteLine("El Resultado de la Division (in16) es: " + divisionint16);
+
+
+
+            // Separador de operaciones
+            Console.WriteLine("====================================");
+
+
+
+
+            //Operaciones con Enteros (byte)
+
+            //Suma
+            byte sumabyte = (byte)(8 + 5);
+            Console.WriteLine("El Resultado de la Suma (byte) es: " + sumabyte);
+
+            //Resta
+            byte restabyte = (byte)(8 - 2);
+            Console.WriteLine("El Resultado de la Resta (byte) es: " + restabyte);
+
+            //Multplicacion
+            byte multiplicacionbyte = (byte)(8 * 9);
+            Console.WriteLine("El Resultado de la Multiplicacion (byte) es: " + multiplicacionbyte);
+
+            //Suma
+            byte divisionbyte = (byte)(30 / 9);
+            Console.WriteLine("El Resultado de la Division (byte) es: " + divisionbyte);
+
+        }
+    }
+}
+
