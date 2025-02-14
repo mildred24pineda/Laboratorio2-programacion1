7c5a6b242cedfee0b4eb91a6c06f81ded694d63e
mildred24pineda <mpinedap3@miumg.edu.gt>
2025-02-13 23:42:38 -0600

    Add files via upload

---
+++ Parte1 Declaraciones y usos de Variables.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Parte1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroEntero = 7;
+            Console.WriteLine("El Numero es: " + numeroEntero);
+            double numeroDecimal = 12.5;
+            Console.WriteLine("El Numero es: " + numeroDecimal);
+            string texto = "Hola, Mucho Gusto es mi Primer Proyecto en C#";
+            Console.WriteLine("El Texto dice: " + texto);
+            bool esFalso = false;
+            Console.WriteLine("El Valor es: " + esFalso);
+
+
+
+        }
+    }
+}
